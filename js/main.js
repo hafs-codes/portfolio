6236c5bc51e3aa9f1b9f41e6e3e28f85319a98d4
Hafsa Omar <hafswa254@gmail.com>
2024-01-21 22:24:38 +0300

    style: adjust font and position in Projects section

--- js/main.js
+++ js/main.js
@@ -1,47 +1,51 @@
 document.addEventListener("DOMContentLoaded", function() {
-  let currentIndex = 0;
-  const items = document.querySelectorAll(".projectItem");
-  const leftArrow = document.getElementById("left-arrow");
-  const rightArrow = document.getElementById("right-arrow");
-
-  function showImage(index) {
-    items.forEach((item, i) => {
-      item.style.display = i === index ? "block" : "none";
+    let currentIndex = 0;
+    const items = document.querySelectorAll(".projectItem");
+    const leftArrow = document.getElementById("left-arrow");
+    const rightArrow = document.getElementById("right-arrow");
+  
+    function showImage(index) {
+      items.forEach((item, i) => {
+        item.style.display = i === index ? "block" : "none";
+      });
+    }
+  
+    function nextImage() {
+      currentIndex = (currentIndex + 1) % items.length;
+      showImage(currentIndex);
+    }
+  
+    function prevImage() {
+      currentIndex = (currentIndex - 1 + items.length) % items.length;
+      showImage(currentIndex);
+    }
+  
+    let intervalId = setInterval(nextImage, 5000);
+    leftArrow.addEventListener("click", function() {
+      clearInterval(intervalId);
+      prevImage();
+      intervalId = setInterval(nextImage, 5000); 
     });
-  }
-
-  function nextImage() {
-    currentIndex = (currentIndex + 1) % items.length;
+  
+    rightArrow.addEventListener("click", function() {
+      clearInterval(intervalId);
+      nextImage();
+      intervalId = setInterval(nextImage, 5000);
+    });
+  
+    const container = document.querySelector(".ProjectsFlex");
+    container.addEventListener("mouseenter", function() {
+      clearInterval(intervalId);
+    });
+  
+    container.addEventListener("mouseleave", function() {
+      intervalId = setInterval(nextImage, 5000);
+    });
+  
     showImage(currentIndex);
+  });
+  
+  function scrollToProjects() {
+      document.getElementById('Projects').scrollIntoView({ behavior: 'smooth' });
   }
-
-  function prevImage() {
-    currentIndex = (currentIndex - 1 + items.length) % items.length;
-    showImage(currentIndex);
-  }
-
-  const intervalId = setInterval(nextImage, 4000);
-  leftArrow.addEventListener("click", function() {
-    clearInterval(intervalId);
-    prevImage();
-  });
-
-  rightArrow.addEventListener("click", function() {
-    clearInterval(intervalId);
-    nextImage();
-  });
-  const container = document.querySelector(".ProjectsFlex");
-  container.addEventListener("mouseenter", function() {
-    clearInterval(intervalId);
-  });
-
-  container.addEventListener("mouseleave", function() {
-    intervalId = setInterval(nextImage, 4000);
-  });
-
-  showImage(currentIndex);
-});
-
-function scrollToProjects() {
-    document.getElementById('Projects').scrollIntoView({ behavior: 'smooth' });
-  }
+  
